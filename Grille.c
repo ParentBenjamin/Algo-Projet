f27ef57622892b5d609dc9f5e98bcb705547c872
anthoback <44365380+anthoback@users.noreply.github.com>
2019-10-10 16:20:40 +0200

    Update Grille.c

--- Grille.c
+++ Grille.c
@@ -16,12 +16,8 @@ int init(int longueur , int largeur , TAB t){ //retourne 1 si l'initiation a ete
     return 0;
 }
 
-int modifValeur(int x , int y , int valeur , TAB t){ //retourne 1 si la modification a ete fait sinon 0
-    if(x <10 && y <10) {
+void modifValeur(int x , int y , int valeur , TAB t){ //retourne 1 si la modification a ete fait sinon 0
         t[x - 1][y - 1].valeur = valeur;
-        return 1 ;
-    }
-    return 0;
 }
 
 int changementEtat(int x , int y , TAB t,int etat){ //change l'etat de la case selectionner et reourne 0 si l'etat demander correspont a l'etat actuel
@@ -32,12 +28,11 @@ int changementEtat(int x , int y , TAB t,int etat){ //change l'etat de la case s
     else return 0;
 }
 
-
 void affiche(TAB t){
 
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-               printf("%d",t[i][j].etat);
+               //printf("%d ",t[i][j].etat);
                printf("%d   ",t[i][j].valeur);
             }
             printf("\n");
