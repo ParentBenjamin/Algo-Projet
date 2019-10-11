2f0640836c9212f4c4eea653cff305426513f92f
juliewatier <56157785+juliewatier@users.noreply.github.com>
2019-10-11 14:33:07 +0200

    Update tiragee.c

--- tiragee.c
+++ tiragee.c
@@ -5,7 +5,7 @@
 void initialisation(Tirage *t) //initialistaion des tirages, nombre et tableau de tirage
 {
     t->nbtirage=0;
-    for (int i=0;i<100;i++)
+    for (int i=0;i<90;i++)
     {
         t->tab[i]=0;
     }
@@ -31,12 +31,28 @@ void affichagetableau(Tirage t)//affichage du tableau de tirage et du nombr de t
 int numtire(Tirage *t) //numero tiree aleatoirement
 {
     int numero;
-    numero = (rand()%99)+ 1; // tire un entier aléatoire entre 1 et 99
+    numero = (rand()%90)+ 1; // tire un entier alÃ©atoire entre 1 et 99
     for(int i=0;i<t->nbtirage;i++)
     {
         if(t->tab[i]==numero)return numtire(t); //si le numero a deja ete tire present dans le tableau on refait le tirage
     }
     return numero;
+}
+
+void general()
+{
+    srand(time(NULL));//eviter la redondance des sequences d'aleatoire;
+    Tirage t;
+    initialisation(&t);//initialisation du tirage
+    int num;
+    for (int i=0;i<90;i++)
+    {
+        num=numtire(&t);
+        majtirage(num,&t);
+        printf("Le numero est : %d\n",num);//affichage du numero tire
+        Sleep(7000); //attente entre deux tirages de 7s
+    }
+    //affichagetableau(t);
 }
 
 #endif // TIRAGE_C_INCLUDED
