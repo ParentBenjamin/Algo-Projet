fd9a2ea7bfe7e9b29eff95825e6dc43c9ffdfeda
juliewatier <56157785+juliewatier@users.noreply.github.com>
2019-10-23 13:50:58 +0200

    Update plateau.h

--- plateau.h
+++ plateau.h
@@ -1,11 +1,18 @@
+#ifndef PLATEAU_H_INCLUDED
+#define PLATEAU_H_INCLUDED
+#include <stdbool.h>
+#include "Grille.h"
 
-#ifndef PROJETALGO_PLATEAU_H
-#define PROJETALGO_PLATEAU_H
+typedef struct
+{
+    int nbcoule;
+}Joueur;
 
-#include <stdlib.h>
-#include <stdio.h>
-#include "grille.h"
+Joueur initialisationjoueur();//initialise le nb de bateau coule a 0
 
+int tir(TAB t, int x, int y, Joueur j);//retourne 0 si a l'eau, 1 si touche, 2 si coule
+int coule(int x, int y,TAB t);//retourne 1 si bateau coule 0 sinon
 
+bool victoire(Joueur j);//defini si un joueur a gagne ou non 
+#endif // PLATEAU_H_INCLUDED
 
-#endif
