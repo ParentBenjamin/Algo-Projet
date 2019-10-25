d265adb2cedd8af4e8ce6f6e4f9afe4f2a0f8cba
anthoback <44365380+anthoback@users.noreply.github.com>
2019-10-25 15:34:17 +0200

    Update Poker.c

--- Poker.c
+++ Poker.c
@@ -1,30 +1,31 @@
 
 #include "Poker.h"
 
-int nbAlea(int n){
+int nbAlea(int n){  //genere une valeur aleatoire entre 0 et n;
     srand (time (NULL));
     return (rand() % n);
 }
 
-joueur initJoueur(){
-    joueur j;
+joueurPoker initJoueur(){  //  initialise un joueur
+    joueurPoker j;
     j.argent = 1500;
     j.jeu[0].valeur=0;
     j.jeu[1].valeur=0;
     j.jeu[0].etat=0;
     j.jeu[1].etat=0;
     j.mise = 0;
+    j.coucher = false;
     return j;
 }
 
-void initTirage( int n ,Case t[5+2*n]){
+void initTirage( int n ,Case t[5+2*n]){  // initialise le tableau de tirage de toutes les cartes qui vont constituer une manche
     for (int i = 0; i < 5+2*n; ++i) {
         t[i].etat = 0;
         t[i].valeur = 0;
     }
 }
 
-void initParti(int n,Parti p){
+void initParti(int n,Parti p){    // initialise la parti
     for (int i = 0; i < 5; ++i) {
         p.river[i].etat = 0;
         p.river[i].valeur = 0;
@@ -35,11 +36,11 @@ void initParti(int n,Parti p){
     p.pot = 0;
 }
 
-void remplirTirage(int n ,Case t[5+2*n]){
+void remplirTirage(int n ,Case t[5+2*n]){    // Fait le tirage de toutes qui vont constituer une manche
     int valeur ,couleur;
     int i = 0;
     while (i < 5+n*2) {
-        valeur = nbAlea(13) + 1;
+        valeur = nbAlea(13) + 2;
         couleur = nbAlea(4) + 1;
         if (!appartient(valeur, couleur, i, t)){
             t[i].valeur = valeur;
@@ -49,7 +50,7 @@ void remplirTirage(int n ,Case t[5+2*n]){
     }
 }
 
-bool appartient(int v, int c ,int n ,Case t[5+2*n]){
+bool appartient(int v, int c ,int n ,Case t[5+2*n]){   // verifie si la carte n'a pas deja etait sorti
     for (int i = 0; i < n ; ++i) {
         if(t[i].etat == c && t[i].valeur == v) {
             return true;
