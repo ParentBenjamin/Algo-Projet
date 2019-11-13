23fd80ac17841d33c6ba734586f438d2efbc0c52
anthoback <44365380+anthoback@users.noreply.github.com>
2019-11-13 16:10:08 +0100

    Update Poker.h

--- Poker.h
+++ Poker.h
@@ -23,20 +23,27 @@ typedef struct {
 
 
 
-void initJoueur(joueurPoker j);
+joueurPoker initJoueur(joueurPoker j);
 void initTirage(int n, Case t[5+2*n]);
-void initParti(int n,Parti p);
+Parti initParti(int n,Parti p);
 int nbAlea(int n);
 bool appartient(int v, int c ,int n ,Case t[5+2*n]);
 void remplirTirage(int n ,Case t[5+2*n]);
-void distribution(int n, Case t[5+2*n], Parti p);
-int maxMise(Parti p);
-void Suivre(Parti p,int joueur);
-void tapis(Parti p , int joueur);
-void ceCoucher(Parti p, int joueur);
-int relancer(Parti p ,int joueur, int somme );
-int quitterLaTable(Parti p, int numeroJoueur);
-void repartitionArgent (Parti p, int joueurGagnant);
-void repartitionEgaliter(Parti p, int n);
-void miseDepart(Parti p, int n, int tour);
+Parti distribution(int n, Case t[5+2*n], Parti p);
+int maxMise(Parti p,int n);
+Parti Suivre(Parti p,int joueur,int n);
+Parti tapis(Parti p , int joueur);
+Parti seCoucher(Parti p, int joueur);
+Parti relancer(Parti p ,int joueur, int somme,int n);
+int quitterLaTable(Parti* p, int numeroJoueur);
+Parti repartitionArgent (Parti p, int joueurGagnant, int n);
+Parti repartitionEgaliter(Parti p, int n);
+Parti miseDepart(Parti p, int n, int tour);
+int potTotale(Parti p,int n);
+int nombreAbbandon(Parti p, int n);
+int nombreCoucher(Parti p, int n);
+Parti tourJoueur(Parti p,int numerojoueur, int n);
+int tour(Parti* p, int n, int premier);
+Parti tourPartie (Parti p , int n,  Case t[5+2*n]);
+void partie();
 #endif //PROJETALGO_POKER_H
