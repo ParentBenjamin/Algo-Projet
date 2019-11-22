98e750761304e20a3737fd8f6204c838d41ac64f
juliewatier <56157785+juliewatier@users.noreply.github.com>
2019-11-22 15:57:19 +0100

    Update batailleNavale.c

--- batailleNavale.c
+++ batailleNavale.c
@@ -2,6 +2,73 @@
 #include "grille.h"
 #include "iaBN.h"
 
+void sauvegarderjeu(TAB joueur, TAB ia, char *nom, Player p)
+{
+    FILE *f=NULL;
+    f=fopen(nom,"w");//ouverture du fichier en ecriture
+    if(f!=NULL)
+    {
+        //copie de la grille valeur joueur
+        for (int i=0; i<9 ; i++)
+        {
+            for (int j=0; j<9; j++)
+            {
+                fprintf(f,"%d",joueur[i][j].valeur);
+            }
+        }
+        //copie de la grille etat joueur
+        for (int i=0; i<9 ; i++)
+        {
+            for (int j=0; j<9; j++)
+            {
+                fprintf(f,"%d",joueur[i][j].etat);
+            }
+        }
+        //copie de la grille valeur ia
+        for (int i=0; i<9 ; i++)
+        {
+            for (int j=0; j<9; j++)
+            {
+                fprintf(f,"%d",ia[i][j].valeur);
+            }
+        }
+        //copie de la grille etat ia
+        for (int i=0; i<9 ; i++)
+        {
+            for (int j=0; j<9; j++)
+            {
+                fprintf(f,"%d",ia[i][j].etat);
+            }
+        }
+        fprintf(f,"%d",p.nbtir);//sauvegarde du nombre de tir du joueur
+    }
+    else printf("erreur");
+    fclose(f);
+}
+
+void genererpartie(TAB chainejoueur, TAB chaineia, Player p)//recupere la partie a partir du fichier texte
+{
+    FILE* fichier =NULL;
+    fichier=fopen("SAUVEGARDEPARTIE.txt","r");//ouverture en lecture
+    if(fichier!=NULL)
+    {
+        for(int i=0;i<9;i++)//copie valeur joueur
+            for(int j=0;j<9;j++)
+                chainejoueur[i][j].valeur=(int)fgetc(fichier)-48;//on afit -48 par rapport au code ASCII
+        for(int k=0;k<9;k++)//copie etat joueur
+            for(int l=0;l<9;l++)
+                chainejoueur[k][l].etat=(int)fgetc(fichier)-48;
+        for(int m=0;m<9;m++)//copie valeur ia
+            for(int n=0;n<9;n++)
+                chaineia[m][n].valeur=(int)fgetc(fichier)-48;
+        for(int o=0;o<9;o++)//copie etat ia
+            for(int p=0;p<9;p++)
+                chaineia[o][p].etat=(int)fgetc(fichier)-48;
+        p.nbtir=(int)fgetc(fichier)-48;
+    }
+    fclose(fichier);
+}
+
 //Verifie victoire, renvoie 1 si true 0 false
 int checkVictoire(TAB t) {
     int victoire = 1;
@@ -15,27 +82,16 @@ int checkVictoire(TAB t) {
     return victoire;
 }
 
-//calcule le score si victoire sinon 0
-int calculScoreBN(Player *p, int fin) {
-    if (fin==1) {
-        return (100-p->nbtir)*2;
-    } else {
-        return 0;
-    }
-}
-
 //fonction principale de la Bataille Navale
 void batailleNavale() {
     srand(time(NULL)); // Necessaire pour la generation aleatoire (ligne a executer 1 seule fois, attention elle est executer dans le loto)
 
     //INITIALISATION
     IA irobot;
-    Player p1;
     TAB tabJOUEUR, tabIA;
     init(9,9,tabJOUEUR);
     init(9,9,tabIA);
     initIA(&irobot);
-    initPlayer(&p1);
 
     //PLACEMENT DES BATEAUX
     placementsDesBateauxJOUEUR(tabJOUEUR);
@@ -46,7 +102,7 @@ void batailleNavale() {
     int fin = 0;
     while (fin==0) {
         //TOUR JOUEUR 1
-        fin = tirJOUEUR(tabIA, tabJOUEUR, &p1);
+        fin = tirJOUEUR(tabIA, tabJOUEUR);
         if (fin==0) {
             fin = tirIA(tabJOUEUR, &irobot);
             if (fin==1) {
@@ -60,12 +116,6 @@ void batailleNavale() {
     } else {
         printf("\nDefaite  !\n");
     }
-
-    int score = calculScoreBN(&p1, fin);
-    printf("Votre score est de : %d points",score);
-    enregistrerScore(score);
-
-
 
     afficheEtat(tabJOUEUR);
 }
