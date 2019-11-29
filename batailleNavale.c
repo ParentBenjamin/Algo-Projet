23b16453d5e5734e6c4b6a1b2946217c26445a34
juliewatier <56157785+juliewatier@users.noreply.github.com>
2019-11-29 13:21:00 +0100

    Update batailleNavale.c

--- batailleNavale.c
+++ batailleNavale.c
@@ -2,11 +2,11 @@
 #include "grille.h"
 #include "iaBN.h"
 
-void sauvegarderjeu(TAB joueur, TAB ia, char *nom, Player p)
+void sauvegarderjeu(TAB joueur, TAB ia, char *nom, Player p)//permet de sauvegarder la partie en cours dans un fichier
 {
     FILE *f=NULL;
     f=fopen(nom,"w");//ouverture du fichier en ecriture
-    if(f!=NULL)
+    if(f!=NULL)//si le fichier est bien ouvert
     {
         //copie de la grille valeur joueur
         for (int i=0; i<9 ; i++)
@@ -40,31 +40,36 @@ void sauvegarderjeu(TAB joueur, TAB ia, char *nom, Player p)
                 fprintf(f,"%d",ia[i][j].etat);
             }
         }
-        fprintf(f,"%d",p.nbtir);//sauvegarde du nombre de tir du joueur
+        //sauvegarde du nombre de tir du joueur
+        fprintf(f,"%d",p.nbtir);
     }
-    else printf("erreur");
-    fclose(f);
+    else printf("erreur");//sinon affichage dune erreur
+    fclose(f);//fermeture du fichier
 }
 
 void genererpartie(TAB chainejoueur, TAB chaineia, Player p)//recupere la partie a partir du fichier texte
 {
     FILE* fichier =NULL;
-    fichier=fopen("SAUVEGARDEPARTIE.txt","r");//ouverture en lecture
+    fichier=fopen("SAUVEGARDEPARTIE.txt","r");//ouverture en lecture du fichier
     if(fichier!=NULL)
     {
-        for(int i=0;i<9;i++)//copie valeur joueur
+        //copie valeur joueur
+        for(int i=0;i<9;i++)
             for(int j=0;j<9;j++)
-                chainejoueur[i][j].valeur=(int)fgetc(fichier)-48;//on afit -48 par rapport au code ASCII
-        for(int k=0;k<9;k++)//copie etat joueur
+                chainejoueur[i][j].valeur=(int)fgetc(fichier)-48;//le -48 permet de recuperer la bonne valeur cf code ASCII
+        //copie etat joueur
+        for(int k=0;k<9;k++)
             for(int l=0;l<9;l++)
                 chainejoueur[k][l].etat=(int)fgetc(fichier)-48;
-        for(int m=0;m<9;m++)//copie valeur ia
+        //copie valeur ia
+        for(int m=0;m<9;m++)
             for(int n=0;n<9;n++)
                 chaineia[m][n].valeur=(int)fgetc(fichier)-48;
-        for(int o=0;o<9;o++)//copie etat ia
+        //copie etat ia
+        for(int o=0;o<9;o++)
             for(int p=0;p<9;p++)
                 chaineia[o][p].etat=(int)fgetc(fichier)-48;
-        p.nbtir=(int)fgetc(fichier)-48;
+        p.nbtir=(int)fgetc(fichier)-48;//copie du nombre de tir du joueur
     }
     fclose(fichier);
 }
