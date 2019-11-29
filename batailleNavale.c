904731d623c13a6c01678e620ad27a7a1bdefdcc
juliewatier <56157785+juliewatier@users.noreply.github.com>
2019-11-29 15:41:46 +0100

    Update batailleNavale.c

--- batailleNavale.c
+++ batailleNavale.c
@@ -2,8 +2,9 @@
 #include "grille.h"
 #include "iaBN.h"
 
-void sauvegarderjeu(TAB joueur, TAB ia, char *nom, Player p, int etape)//permet de sauvegarder la partie en cours dans un fichier
+void sauvegarderjeu(TAB joueur, TAB ia, char *nom, Player p, int etape, int compt)//permet de sauvegarder la partie en cours dans un fichier
 {
+    int taillenbtir=1;
     FILE *f=NULL;
     f=fopen(nom,"w");//ouverture du fichier en ecriture
     if(f!=NULL)//si le fichier est bien ouvert
@@ -41,15 +42,22 @@ void sauvegarderjeu(TAB joueur, TAB ia, char *nom, Player p, int etape)//permet 
             }
         }
         //sauvegarde du nombre de tir du joueur
-        fprintf(f,"%d",p.nbtir);
-        fprintf(f,"%d",etape);
+       if(p.nbtir>9)taillenbtir=2;
+       fprintf(f,"%d",taillenbtir);
+       fprintf(f,"%d",p.nbtir);
+       fprintf(f,"%d",etape);
+       fprintf(f,"%d",compt);
     }
     else printf("erreur");//sinon affichage dune erreur
     fclose(f);//fermeture du fichier
 }
 
-int genererpartie(TAB chainejoueur, TAB chaineia, Player p)//recupere la partie a partir du fichier texte
+int genererpartie(TAB chainejoueur, TAB chaineia, Player p)//recupere la partie a partie du fichier texte retourne nombre positif=numero etape ou nombre negatif=compteur si etape1
 {
+    int compt;//compteur pour le nbre de bateau
+    int taille;//permet de recuperer la taille en nbre d'unite du nombre de tir du joueur
+    int lire1; 
+    int lire2;
     int etape;//variable pour recuperer a quel moment a été arreté la partie
     FILE* fichier =NULL;
     fichier=fopen("SAUVEGARDEPARTIE.txt","r");//ouverture en lecture du fichier
@@ -71,11 +79,20 @@ int genererpartie(TAB chainejoueur, TAB chaineia, Player p)//recupere la partie 
         for(int o=0;o<9;o++)
             for(int p=0;p<9;p++)
                 chaineia[o][p].etat=(int)fgetc(fichier)-48;
-        p.nbtir=(int)fgetc(fichier)-48;//copie du nombre de tir du joueur
+        taille=(int)fgetc(fichier)-48;//copie de la taille du nb tir
+        if(taille==1)p.nbtir=(int)fgetc(fichier)-48;//copie du nombre de tir du joueur
+        else 
+        {
+            lire1=(int)fgetc(fichier)-48;
+            lire2=(int)fgetc(fichier)-48;
+            p.nbtir=lire1*10 + lire2;
+        }        
         etape=(int)fgetc(fichier)-48;//copie du numero detape pour savoir quand est ce que letape a ete arretee
+        compt=(int)fgetc(fichier)-48;//copie du compteur : compteur qui sert a savoir combien de bateau a deja été placé
     }
     fclose(fichier);
-    return etape;
+    if(etape==1)return -compt;
+    else return etape;
 }
 
 //Verifie victoire, renvoie 1 si true 0 false
