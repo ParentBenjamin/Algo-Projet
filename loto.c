3c2ffd62a6c18556fcf50cde862e28a6b449f4e5
juliewatier <56157785+juliewatier@users.noreply.github.com>
2019-12-12 20:36:31 +0100

    Update loto.c

--- loto.c
+++ loto.c
@@ -1,7 +1,85 @@
 #include "loto.h"
 #include "tiragee.h"
 
+//mise a letat 3 de la case du tableu si le numéro est touché
+void majetat (TAB t, int nbCartons, int nombre,int choix)
+{
+    if (nombreTrouve(t,nbCartons,nombre)&&choix==1)
+    {
+        int yEnd = nbCartons*3-1; //limite dans le tableau en fonction du nombre de cartons utilises
+        int col;
+        if (nombre==90)col = 8;
+        else col = nombre/10;
 
+        for (int i=0; i<=yEnd; i++) //on passe a letat 3 le nbre
+        {
+            if (t[i][col].valeur==nombre) changementEtat(i+1,col+1,t,3);
+        }
+    }
+}
+
+//calcul du score du joueur
+/*si grille remplie : 200 - nbtirages (soit minimum 110 points = grille remplie) OK
+donc une ligne = 37 points
+si grille non remplie et partie terminée -> si 1 ligne : 110-74
+					 -> si 2 lignes : 110-37
+					 -> 0 ligne et 7 ou - numéros :0
+					 -> 0 ligne et 8 ou + numéros :18
+On fait ce calcul pour chaque grille et on fait une moyenne par rapport au nombre de grille*/
+
+
+//fonction qui calcule le score pour une grille
+int score(TAB t, int dep, Tirage tir)//la variable dep sert de numero de depart de ligne si premiere grille dep=0, si 2eme grille dep=3..
+{
+    int total;
+    int somme;
+    int nbligne=0;
+    int nbnum=0;
+    for(int i=dep;i<(dep+3);i++)//i = numero de ligne
+    {
+        somme=0;
+        for(int j=0;j<9;j++)//j=numero de colonne
+        {
+            if(t[i][j].etat==3)somme=somme+1;//compte le nombre de numero sur la ligne
+        }
+        nbnum=nbnum+somme;//nbnum est le nombre de num touchés au total sur la grille
+        if(somme==5)nbligne=nbligne +1;//la ligne est remplie
+    }
+    if(nbligne==3)total=200-tir.nbtirage;//la grille est remplie
+    if(nbligne==2)total=73;
+    if(nbligne==1)total=36;
+    if(nbligne==0)
+    {
+        if (nbnum<=7)total=0;
+        else if(nbnum>=8)total=18;
+    }
+    return total;
+}
+
+int scorefinal(TAB t,int nbCartons, Tirage tir)
+{
+    int res;
+    if(nbCartons==1)res=score(t,0,tir);
+    if(nbCartons==2)
+    {
+        int res1;
+        int res2;
+        res1=score(t,0,tir);
+        res2=score(t,4,tir);
+        res=(res1+res2)/2;
+    }
+    if(nbCartons==3)
+    {
+        int res1;
+        int res2;
+        int res3;
+        res1=score(t,0,tir);
+        res2=score(t,3,tir);
+        res3=score(t,6,tir);
+        res=(res1+res2+res3)/3;
+    }
+    return res;
+}
 //Verifie pour une, deux ou trois grilles qu'elles contiennent uniquement des nombres contenus dans le tableau des tirages effectues   1 si vrai    0 si faux
 int verifierVictoire(TAB t, int nbCartons) {
     int yEnd = nbCartons*3-1;
@@ -25,7 +103,7 @@ int estDejaPresent(TAB t, int yStart, int yEnd, int nombre, int col) {
     return 0;
 }
 
-//Verifie si la colonne o� doit �tre place le nombre est pleine ou pas   1 si vrai 0 sinon
+//Verifie si la colonne où doit être place le nombre est pleine ou pas   1 si vrai 0 sinon
 int colonneRemplie(TAB t, int yStart, int yEnd, int nombre, int col) {
     for (int i=yStart; i<=yEnd; i++) {
         if (t[i][col].valeur == 0) {
@@ -56,9 +134,9 @@ int placeDispoValide(TAB t, int yStart, int yEnd, int col) {
     return 0;
 }
 
-//Genere une grille avec toutes les contraintes du loto   (retourne 1 si generation effectu�e, 0 sinon)
+//Genere une grille avec toutes les contraintes du loto   (retourne 1 si generation effectuée, 0 sinon)
 int generer(TAB t, int yStart, int yEnd) { //yStart et yEnd entiers qui delimitent la partie du tableau a generer (ex 0 et 2 si une seule grille)
-    if (yEnd - yStart == 2) { //verification car on genere une grille � la fois
+    if (yEnd - yStart == 2) { //verification car on genere une grille à la fois
         int cpt = 0; //nombre de nombres places dans la grille
         while (cpt<15) {
             int nbr = getRandom(1,90);
